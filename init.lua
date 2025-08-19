b8afbe99bd6ade4827d4c23d960703a97f942a37
Anton Baranov <abaranov@linuxfoundation.org>
2025-08-19 07:45:44 -0400

    feat(init): enhance config with new plugins, Neovide settings, and theme updates
    - Removed Kickstart introduction block for a cleaner init.lua.
    - Enabled Nerd Font support and adjusted scrolloff for improved usability.
    - Added Neovide-specific GUI settings.
    - Integrated Telescope Project and Tabs extensions with keymaps.
    - Added Neogit, Copilot, and CodeCompanion plugins for Git, AI, and coding assistance.
    - Switched colorscheme to modus_operandi and added modus-themes.nvim.
    - Enabled linting, Neo-tree, and Gitsigns plugins.
    - Added Terraform LSP configuration.
    - Updated Treesitter to include HCL and Nix languages.
    - Fixed typo in Lua LSP settings.
    - Miscellaneous improvements and new keymaps.
    
    Signed-off-by: Anton Baranov <abaranov@linuxfoundation.org>

--- init.lua
+++ init.lua
@@ -1,89 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -91,7 +5,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -159,12 +73,16 @@ vim.o.inccommand = 'split'
 vim.o.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 10
+vim.o.scrolloff = 5
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+
+-- Put a dollar sign at the end of word when you change it
+local current_cpoptions = vim.o.cpoptions
+vim.o.cpoptions = current_cpoptions .. '$'
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -204,6 +122,18 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+
+-- [[ Neovide Configurations ]]
+if vim.g.neovide then
+  vim.o.guifont = 'Iosevka Nerd Font:h14'
+  vim.o.linespace = 0
+  vim.g.neovide_scale_factor = 1.0
+  vim.g.neovide_hide_mouse_when_typing = true
+  -- Disable cursor animation
+  vim.g.neovide_cursor_animation_length = 0
+  -- Disable scroll animation
+  vim.g.neovide_scroll_animation_length = 0
+end
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -380,6 +310,9 @@ require('lazy').setup({
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      -- Telescope Project is a plugin that allows you to quickly switch between projects.
+      { 'nvim-telescope/telescope-project.nvim' },
+      { 'LukasPietzschmann/telescope-tabs' },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -417,12 +350,28 @@ require('lazy').setup({
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
           },
+          ['project'] = {
+            base_dirs = {
+              { path = '~/work/repos', max_depth = 1 },
+              { path = '~/presonal/repos', max_depth = 1 },
+            },
+            hidden_files = true,
+          },
+          ['telescope-tabs'] = {
+            show_preview = true,
+            tabs_label = 'Tabs',
+            current_tab_label = 'Current Tab',
+            new_tab_label = '+ New Tab',
+            close_tab_label = 'x Close Tab',
+          },
         },
       }
 
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
+      pcall(require('telescope').load_extension, 'project')
+      pcall(require('telescope').load_extension, 'telescope-tabs')
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
@@ -459,6 +408,14 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[S]earch [N]eovim files' })
+      -- Shortcut to seach projects with Telescope project extension
+      vim.keymap.set('n', '<leader>sp', function()
+        require('telescope').extensions.project.project { hidden = true }
+      end, { desc = '[S]earch [P]rojects' })
+      -- Shorcut to works with tabs
+      vim.keymap.set('n', '<leader>st', function()
+        require('telescope-tabs').list_tabs()
+      end, { desc = '[S]earch [T]abs' })
     end,
   },
 
@@ -690,13 +647,18 @@ require('lazy').setup({
           -- capabilities = {},
           settings = {
             Lua = {
-              completion = {
+              cuompletion = {
                 callSnippet = 'Replace',
               },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
               -- diagnostics = { disable = { 'missing-fields' } },
             },
           },
+        },
+        terraformls = {
+          cmd = { 'terraform-ls', 'serve' },
+          filetypes = { 'terraform', 'terraform-vars' },
+          root_markers = { '.terraform', '.git' },
         },
       }
 
@@ -875,7 +837,8 @@ require('lazy').setup({
       signature = { enabled = true },
     },
   },
-
+  -- Modus-operandi theme - my favoritecolor theme
+  { 'miikanissi/modus-themes.nvim', priority = 1000 },
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
@@ -894,7 +857,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'modus_operandi'
     end,
   },
 
@@ -944,7 +907,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'hcl', 'nix' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -975,10 +938,10 @@ require('lazy').setup({
   --
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
@@ -990,6 +953,125 @@ require('lazy').setup({
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
+  --  An interactive and powerful Git interface for Neovim, inspired by Magit
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+      'nvim-telescope/telescope.nvim', -- optional
+    },
+    config = function()
+      require('neogit').setup {
+        integrations = {
+          telescope = true,
+        },
+        highlight = {
+          italic = true,
+          bold = true,
+          underline = true,
+        },
+        commit_view = {
+          kind = 'vsplit',
+          verify_commit = vim.fn.executable 'gpg' == 1, -- Can be set to true or false, otherwise we try to find the binary
+        },
+      }
+      vim.keymap.set('n', '<leader>vg', vim.cmd.Neogit, {})
+    end,
+  },
+  {
+    'github/copilot.vim',
+    lazy = false,
+    -- confige = function()
+    --   vim.g.copilot_filetypes = {
+    --     ['*'] = true,
+    --   }
+    --   vim.g.copilot_no_tab_map = true
+    --   vim.set.keymap('i', '<C-c><C-a>', 'copilot#Accept("<CR>")', {})
+    --   vim.set.keymap('i', '<C-c><C-r>', 'copilot#Dismiss()', {})
+    --   vim.set.keymap('i', '<C-L>', 'copilot#Next()', {})
+    --   vim.set.keymap('i', '<C-H>', 'copilot#Previous()', {})
+    --   vim.set.keymap('i', '<C-Space>', 'copilot#Refresh()', {})
+    -- end,
+  },
+  {
+    'olimorris/codecompanion.nvim',
+    opts = {},
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require('codecompanion').setup {
+        assitants = {
+          opencode = {
+            name = 'OpenCode',
+            description = 'Open code files in the current project',
+            endpoint = 'http://127.0.0.1:5000',
+          },
+        },
+        strategies = {
+          inline = {
+            keymaps = {
+              accept_change = {
+                modes = { n = '<leader>a' },
+                description = 'Accept the suggested change',
+              },
+              reject_change = {
+                modes = { n = '<leader>r' },
+                opts = { nowait = true },
+                description = 'Reject the suggested change',
+              },
+            },
+          },
+          chat = {
+            name = 'copilot',
+            model = 'gpt-4o',
+            keymaps = {
+              send = {
+                modes = { n = '<C-c><C-c>', i = '<C-c><C-c>' },
+                opts = {},
+              },
+              close = {
+                modes = { n = '<C-c><C-k>', i = '<C-c><C-k>' },
+                opts = {},
+              },
+            },
+            slash_commands = {
+              ['file'] = {
+                -- Location to the slash command in CodeCompanion
+                callback = 'strategies.chat.slash_commands.file',
+                description = 'Select a file using Telescope',
+                opts = {
+                  provider = 'telescope', -- Can be "default", "telescope", "fzf_lua", "mini_pick" or "snacks"
+                  contains_code = true,
+                },
+              },
+            },
+          },
+        },
+        vim.keymap.set('n', '<leader>cc', '<cmd>CodeCompanionChat<cr>', { desc = 'AI [C]ode [Companion [C]hat' }),
+        vim.keymap.set('n', '<leader>ca', '<cmd>CodeCompanionActions<cr>', { desc = 'AI [C]ode [Companion [A]ctions' }),
+      }
+    end,
+  },
+  -- {
+  --   'nvim-neo-tree/neo-tree.nvim',
+  --   branch = 'v3.x',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     'MunifTanjim/nui.nvim',
+  --     'nvim-tree/nvim-web-devicons', -- optional, but recommended
+  --   },
+  --   lazy = false, -- neo-tree will lazily load itself
+  --   vim.keymap.set('n', '<leader>e', '<Cmd>Neotree<CR>'),
+  --   config = function()
+  --     require('neo-tree').setup {
+  --       enable_git_status = true,
+  --       enable_diagnostics = true,
+  --     }
+  --   end,
+  -- },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
