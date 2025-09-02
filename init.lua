b6a0abe255b30abe6defe8ecf1be87129a019c1c
Anton Baranov <abaranov@linuxfoundation.org>
2025-09-02 14:16:16 -0400

    feat(nix): use spaces for nix files instead of tabs
    Signed-off-by: Anton Baranov <abaranov@linuxfoundation.org>

--- init.lua
+++ init.lua
@@ -155,6 +155,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'nix',
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
